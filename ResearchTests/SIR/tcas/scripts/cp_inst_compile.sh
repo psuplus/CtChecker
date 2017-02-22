325c0f1d7a01181d8f34079d751bf307c5382fb6
Yu-Tse Lin <yzl5497@psu.edu>
2017-02-22 14:18:06 -0500

    Added new scripts

---
+++ ResearchTests/SIR/tcas/scripts/cp_inst_compile.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+function printUsage {
+  echo "USAGE:"
+  echo "      ./cp_and_compile.sh <version>"
+  echo "      <version> varies from 1 to 41"
+  exit
+}
+
+if [[ $# -lt 1 ]]; then
+  printUsage
+elif [[ $1 -lt 1 ]]; then
+  printUsage
+elif [[ $1 -gt 41 ]]; then
+  printUsage
+fi
+
+ver=$1
+
+cp ../versions.alt/versions.orig/v$ver/tcas.c ../source
+
+cd ../source
+
+# Start instrumentation
+
+## Check virtual environment
+if [ -z $VTENV ]; then
+	echo "
+Run \`source ./env.sh\` in \`ResearchTests\` to activate the virtual environment"
+  exit
+fi
+
+clang -g -O0 -emit-llvm -c tcas.c -o tcas.bc
+
+echo 0 > /tmp/llvm0
+opt -load /home/troy/LLVM-Deps/Debug+Asserts/lib/Research.so -prtLnTrace tcas.bc -o tcas.g.bc &> ../nodes
+
+clang -g -O0 -emit-llvm -c /home/troy/LLVM-Deps/ResearchTests/instrumentation/BBInfo/printLine.cpp
+
+llvm-link tcas.g.bc printLine.bc -o tcas.linked.bc
+
+llc -filetype=obj tcas.linked.bc -o tcas.o
+
+g++ -std=c++11 tcas.o -o tcas.c.inst.exe
---
+++ ResearchTests/SIR/tcas/scripts/test.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+function printUsage {
+  echo "USAGE:"
+  echo "      ./test.sh <version>"
+  echo "      <version> varies from 1 to 41"
+  exit
+}
+
+if [[ $# -lt 1 ]]; then
+  printUsage
+elif [[ $1 -lt 1 ]]; then
+  printUsage
+elif [[ $1 -gt 41 ]]; then
+  printUsage
+fi
+
+# copy the original source code and compile
+
+cp ../source.alt/source.orig/tcas.c ../source
+
+(
+  cd ../source;
+  gcc tcas.c -o tcas.exe
+) &> /dev/null
+
+# run the tests on original code
+## skip for efficiency
+if false; then
+  cd ../scripts
+  echo Running ./runall.sh
+  (./runall.sh) > /dev/null
+  mv -f ../outputs/* ../orgoutputs
+fi
+
+# cp intrument and compile version $1
+echo "Running rm ../traces/*"
+rm ../traces/*
+echo Running ./cp_inst_compile.sh $1
+(./cp_inst_compile.sh $1) &> /dev/null
+
+echo Running ./gettraces.sh
+(./gettraces.sh) > /dev/null
+
+echo Running ./collect_traces.sh
+./collect_traces.sh
+
+# diff the source files to see where has been changed
+echo "diff <original tcas.c> <new tcas.c>"
+diff ../source.alt/source.orig/tcas.c ../versions.alt/versions.orig/v$1/tcas.c
+
+# Generate the output
+echo
+echo Generate the output
+python ../../../generateGraph.py ../nodes ../trace

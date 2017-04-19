2777bea3879a09a47fd56d876e0bc075f8ba29f6
yzl5497 <yzl5497@psu.edu>
2017-04-19 15:31:21 -0400

    changed chmod

--- ResearchTests/SIR/schedule/scripts/getErrorInfo.sh
+++ ResearchTests/SIR/schedule/scripts/getErrorInfo.sh
@@ -4,7 +4,7 @@
 export PROGRAM=schedule
 
 (printf "{\n"
-for f in {1..41}; do
+for f in {1..9}; do
   diff ../source.alt/source.orig/$PROGRAM.c ../versions.alt/versions.orig/v$f/$PROGRAM.c 1>&2
   read error
   echo "$f: ($error,),"

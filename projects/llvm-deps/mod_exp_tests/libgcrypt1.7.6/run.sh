09aead66ee2e816644e7ee5cfe78dfe4cd8dae30
Adam Mohammed <adammoham@gmail.com>
2018-03-23 17:20:36 -0400

    Changed run.sh to fix issue with testing

--- projects/llvm-deps/mod_exp_tests/libgcrypt1.7.6/run.sh
+++ projects/llvm-deps/mod_exp_tests/libgcrypt1.7.6/run.sh
@@ -10,7 +10,7 @@ fi
 CPPFLAGS=
 LLVMLIBS=
 LDFLAGS=
-LEVEL="../../../../.."
+LEVEL="../../../.."
 
 # if your instrumentation code calls into LLVM libraries, then comment out the above and use these instead:
 #CPPFLAGS=`llvm-config --cppflags`
@@ -19,8 +19,8 @@ LEVEL="../../../../.."
 
 ## compile the instrumentation module to bitcode
 ## clang $CPPFLAGS -O0 -emit-llvm -c sample.cpp -o sample.bc
-$LEVEL/Debug+Asserts/bin/clang -isystem include -O0 -g -emit-llvm -o ../mpi-pow.bc -c mpi-pow.c
-$LEVEL/Debug+Asserts/bin/clang -isystem inlcude -O0 -g -emit-llvm -S mpi-pow.c
+$LEVEL/Debug+Asserts/bin/clang -isystem include -I include -O0 -g -emit-llvm -o mpi-pow.bc -c mpi/mpi-pow.c
+$LEVEL/Debug+Asserts/bin/clang -isystem inlcude -I include -O0 -g -emit-llvm -S mpi/mpi-pow.c
 
 ## opt -load *.so -infoflow < $BENCHMARKS/welcome/welcome.bc -o welcome.bc
 $LEVEL/Debug+Asserts/bin/opt  -load $LEVEL/projects/poolalloc/Debug+Asserts/lib/LLVMDataStructure.$EXT \
@@ -29,8 +29,10 @@ $LEVEL/Debug+Asserts/bin/opt  -load $LEVEL/projects/poolalloc/Debug+Asserts/lib/
   -load $LEVEL/projects/llvm-deps/Debug+Asserts/lib/pointstointerface.$EXT \
   -load $LEVEL/projects/llvm-deps/Debug+Asserts/lib/Deps.$EXT  \
   -load $LEVEL/projects/llvm-deps/Debug+Asserts/lib/Security.$EXT  \
-  -vulnerablebranch  -debug < ../mpi-pow.bc > /dev/null
+  -vulnerablebranch  -debug < mpi-pow.bc 2> tmp.dat > /dev/null
 
+export PATH="$PATH:../../processing_tools" # tmp change to path to have post-processing tools
+post_analysis.py tmp.dat > results_with_source.txt
 ## link instrumentation module
 #llvm-link welcome.bc sample.bc -o welcome.linked.bc
 

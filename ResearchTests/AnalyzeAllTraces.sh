bdc40ecee99e1c91519c85954a36537762758234
Yu-Tse Lin <yzl5497@CSE-dynpool05.cse.psu.edu>
2018-03-26 09:20:18 -0400

    modified sh scripts

--- ResearchTests/AnalyzeAllTraces.sh
+++ ResearchTests/AnalyzeAllTraces.sh
@@ -2,7 +2,7 @@
 
 echo $0 $@
 
-function printUsage {
+function printUsage_old {
     echo "Usage: $0 <nodes folder> <traces folder> <error info> \"<options>\""
     echo "      <nodes folder>	folder contains all trace info ([i].nd)"
     echo "      <traces folder>	folder contains all trace info ([i].tr)"
@@ -11,18 +11,38 @@ function printUsage {
     echo '      		  `python skeleton.py -h` to see options'
     exit
 }
-nds=$1
-trs=$2
-err=$3
-opt=$4
+function printUsage {
+    echo "Usage: $0 <benchmark_folder> <traces folder> <error info> \"<options>\""
+    echo "      <nodes folder>	folder contains all trace info ([i].nd)"
+    echo "      <traces folder>	folder contains all trace info ([i].tr)"
+    echo "      <error info>	file contains errorinfo for all versions"
+    echo "      [options] 	options passed into skeleton.py"
+    echo '      		  `python skeleton.py -h` to see options'
+    exit
+}
+nds=$1/n_all
+trs=$1/t_all
+err=$1/errorInfo
+opt=$2
 
 function testing {
-	printf "\n\noption $1\n\n"
+	printf "\n\noption $1, $2\n\n"
 	for (( i = 1 ;  ; i++ )); do
 		if [[ ( -f $nds/$i.nd ) && ( -f $trs/$i.tr ) ]]; then
-			echo
+			if [[ ! -f $trs/$i.tr.s ]]; then
+				echo
+				echo "Getting Compact Info..."
+				python tr_simplify.py $trs/$i.tr 
+				echo "Compact Info Stored"
+			fi
 			echo Start Running Test $i
-			python skeleton.py $nds/$i.nd $trs/$i.tr $err $i $1
+			if [[ $1 == 'node' ]]; then 
+				echo python skeleton.py $nds/$i.nd $trs/$i.tr.s $err $i -b $1 -m $2
+				python skeleton.py $nds/$i.nd $trs/$i.tr.s $err $i -b $1 -m $2
+			else
+				echo python skeleton.py $nds/$i.nd $trs/$i.tr.p $err $i -b $1 -m $2
+				python skeleton.py $nds/$i.nd $trs/$i.tr.p $err $i -b $1 -m $2
+			fi
 		else
 			printf "\n\n\nAll Tests are Finished\n\n\n"
 			break
@@ -30,22 +50,24 @@ function testing {
 	done
 }
 
-if [[ $# -lt 3 ]]; then
+if [[ $# -lt 1 ]]; then
 	printUsage
-elif [[ $# -eq 3 ]]; then
+elif [[ $# -eq 1 ]]; then
+	bases='node edge'
+	methods='const freq'
 
-	options=(
-	  '-b node -m const'
-	  '-b node -m freq'
-	  '-b edge -m const'
-	  '-b edge -m freq'
-	)
-
-	for i in `seq 0 3`; do
-		testing "${options[$i]}"
+	for a in $bases ; do
+		for b in $methods ; do
+			echo "Testing $a $b"
+			testing $a $b
+		done
 	done
 
 else
-	testing "$opt"
+	testing $opt
 
 fi
+
+mkdir -p $1/result_log
+mv /tmp/*.log $1/result_log
+echo "The results are stored in $1/result_log"

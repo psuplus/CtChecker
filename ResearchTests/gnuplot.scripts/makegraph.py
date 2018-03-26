96858bb0a885eb113f74023abe2f5a8395ea5f26
Yu-Tse Lin <yzl5497@CSE-dynpool05.cse.psu.edu>
2018-03-26 09:26:05 -0400

    updated plotting scripts

---
+++ ResearchTests/gnuplot.scripts/makegraph.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import sys, os
+
+f = open(sys.argv[1])
+
+f.readline()
+f.readline()
+s = []
+maxi = 0
+
+for line in f.readlines():
+  a,b,c = line.strip().split("\t")
+  if c[0] != '>': 
+    s.append(int(c))
+    maxi = max(int(c), maxi)
+  else: maxi = max(int(c[1:]), maxi)
+f.close()
+
+s.sort()
+fdir, fname = os.path.split(sys.argv[1])
+
+f = open(fdir+os.path.splitext(fname)[0]+".plot", "w+")
+
+for i in range(len(s)):
+  if i== len(s)-1 or s[i] != s[i+1]:
+    f.write("%f %f\n" % (s[i]/float(maxi), (i+1)/float(len(s))))
+
+
+

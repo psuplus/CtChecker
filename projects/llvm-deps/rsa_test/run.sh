062b602c89b0026aa1e83ef3081a4b9f6da748e3
Adam Mohammed <adammoham@gmail.com>
2018-06-22 17:24:15 -0400

    Updated mod_exp results with complete Bear i32 library

--- projects/llvm-deps/rsa_test/run.sh
+++ projects/llvm-deps/rsa_test/run.sh
@@ -23,7 +23,7 @@ $LEVEL/Debug+Asserts/bin/clang -O0 -g -emit-llvm -o rsa.bc -c rsa.c
 $LEVEL/Debug+Asserts/bin/clang -O0 -g -emit-llvm -o bignum.bc -c bignum.c
 $LEVEL/Debug+Asserts/bin/clang -O0 -g -emit-llvm -S rsa.c
 $LEVEL/Debug+Asserts/bin/clang -O0 -g -emit-llvm -S bignum.c
-$LEVEL/Debug+Asserts/bin/llvm-link rsa.bc bignum.bc -o rsa_mpi.bc
+$LEVEL/Debug+Asserts/bin/llvm-link bignum.bc -o rsa_mpi.bc
 
 ## opt -load *.so -infoflow < $BENCHMARKS/welcome/welcome.bc -o welcome.bc
 $LEVEL/Debug+Asserts/bin/opt  -load $LEVEL/projects/poolalloc/Debug+Asserts/lib/LLVMDataStructure.$EXT \
@@ -32,8 +32,10 @@ $LEVEL/Debug+Asserts/bin/opt  -load $LEVEL/projects/poolalloc/Debug+Asserts/lib/
   -load $LEVEL/projects/llvm-deps/Debug+Asserts/lib/pointstointerface.$EXT \
   -load $LEVEL/projects/llvm-deps/Debug+Asserts/lib/Deps.$EXT  \
   -load $LEVEL/projects/llvm-deps/Debug+Asserts/lib/Security.$EXT  \
-  -vulnerablebranch  -debug < rsa_mpi.bc 2>&1 > /dev/null
+  -vulnerablebranch  -debug < rsa_mpi.bc 2> tmp.dat > /dev/null
 
+export PATH="$PATH:../processing_tools" # tmp change to path to have post-processing tools
+post_analysis.py tmp.dat > results_with_source.txt
 ## link instrumentation module
 #llvm-link welcome.bc sample.bc -o welcome.linked.bc
 

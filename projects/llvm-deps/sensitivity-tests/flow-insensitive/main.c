b7f476615d2c48a871b56f7be91560b3db9f265b
Ernest D DeFoy III <erniedefoy3@gmail.com>
2019-10-15 13:55:27 -0400

    flow insensitive example

---
+++ projects/llvm-deps/sensitivity-tests/flow-insensitive/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+void mbed (int secret)
+{
+	size_t wbits;
+	size_t i;
+	size_t nbits;
+	
+	i = secret;
+	/*
+	 * process the remaining bits
+	 */
+	for( i = 0; i < nbits; i++ )
+	{
+	
+		wbits <<= 1;
+	}
+	
+}
+
+

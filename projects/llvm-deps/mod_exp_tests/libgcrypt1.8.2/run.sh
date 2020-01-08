c232a12382d0fa0faabbd31ba2eb5fcde1edc099
Quan Zhou <qfz5074@psu.edu>
2020-01-07 21:00:01 -0500

    fixing date command on Mac OS

--- projects/llvm-deps/mod_exp_tests/libgcrypt1.8.2/run.sh
+++ projects/llvm-deps/mod_exp_tests/libgcrypt1.8.2/run.sh
@@ -77,7 +77,7 @@ FILE="mpi/mpi-pow.c"
 $LEVEL/Debug+Asserts/bin/clang -isystem include -I include -O0 -g -emit-llvm -o mpi-pow.bc -c mpi/mpi-pow.c
 $LEVEL/Debug+Asserts/bin/clang -isystem inlcude -I include -O0 -g -emit-llvm -S mpi/mpi-pow.c
 
-TIME=$(date +%s.%N)
+TIME=$(date +%s)
 ## opt -load *.so -infoflow < $BENCHMARKS/welcome/welcome.bc -o welcome.bc
 $LEVEL/Debug+Asserts/bin/opt $MEM2REG -load $LEVEL/projects/poolalloc/Debug+Asserts/lib/LLVMDataStructure.$EXT \
   -load $LEVEL/projects/llvm-deps/Debug+Asserts/lib/Constraints.$EXT  \
@@ -86,11 +86,11 @@ $LEVEL/Debug+Asserts/bin/opt $MEM2REG -load $LEVEL/projects/poolalloc/Debug+Asse
   -load $LEVEL/projects/llvm-deps/Debug+Asserts/lib/Deps.$EXT  \
   -load $LEVEL/projects/llvm-deps/Debug+Asserts/lib/Security.$EXT  \
   -vulnerablebranch  -debug < $1 2> tmp.dat > /dev/null
-TIME=$(echo "$(date +%s.%N) - $TIME" | bc)
-printf "Execution time: %.6f seconds\n" $TIME
+TIME=$(echo "$(date +%s) - $TIME" | bc)
+printf "Execution time: %d seconds\n" $TIME
 
 export PATH="$PATH:../../processing_tools" # tmp change to path to have post-processing tools
-post_analysis.py tmp.dat $START $END $COL 3 $FILE $TIME > results_with_source.txt
+post_analysis.py tmp.dat $START $END $COL 1 $FILE $TIME > results_with_source.txt
 
 COL=$( echo 'tmp-'$COL'.dat' | tr '/' '-')
 

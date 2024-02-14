9ada48444b4de935f9537f7a49bf78b3fb68822d
Jeffrey Ching <jcc150@cs.duke.edu>
2024-02-14 13:43:25 -0500

    add bi-directional example

---
+++ projects/llvm-deps/sensitivity-tests/bi-directional/main.c
@@ -0,0 +1,30 @@
+int relabel(int to_be_relabel, int predicate, char* label_dynamic, char* label_static);
+// {
+//   if (predicate) {
+//     return to_be_relabel;
+//   } else {
+//     exit();
+//   }
+// }
+
+int library(int book, int retur) {
+  int note;
+  int Alice;
+  char* label_book = "retur?P <-> S";
+  char* pub_book = "P";
+  char* priv_book="S";
+  //if ()
+  //question mark label
+  //In the analysis transfer ? string to the constraints
+  // public = bid + 1;
+ if (retur==1){
+  Alice = relabel(book, retur, label_book, pub_book) + 1;
+  note = 2;
+  }
+  else{
+ // relabel(book, retur, label_book, priv_book);// Alice can no longer access
+  Alice = note + 1;
+  }
+
+  return Alice;
+}

2731f70256a2d11e1b5f2ef86c3968119c201f4a
Quan Zhou <qfz5074@psu.edu>
2024-07-12 20:35:43 -0400

    Submitting artifact

--- projects/llvm-deps/paper/conference/examples/aes.c
+++
@@ -1,7 +0,0 @@
-void AES_enc(char *in, char *out, AES_KEY *key) {
-
-      unsigned * rk = key->rd_key;
-      if (key->rounds > 10) {
-
-      }
-}
--- projects/llvm-deps/paper/thesis/examples/findoffset.cpp
+++
@@ -1,5 +0,0 @@
-unsigned findOffsetFromFieldIndex(StructType* type, unsigned fieldIdx, AbstractLoc* loc) {
-  DataLayout &TD = loc->getParentGraph()->getDataLayout();
-  StructLayout *SL = TD.getStructLayout(type);
-  return SL->getElementOffset(fieldIdx);
-}
